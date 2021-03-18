1e1ffc18a0f5742865ce7b1468b5faa2e88fdc6e
jose <jabelcastellanosjoo@unm.edu>
2021-03-18 12:54:36 -0600

    Add quick test.

---
+++ z3-interp-plus-test/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../include/z3++.h"
+#include "../include/z3.h"
+
+int main(){
+  z3::context ctx;
+  z3::expr x = ctx.int_const("x");
+  z3::expr y = ctx.int_const("y");
+  z3::expr a = x < y;
+  std::cout << a.qf_to_simplify() << std::endl;
+  return 0;
+}
